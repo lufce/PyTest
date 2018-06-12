36997e43075fc9d2d1be2ee7d3f1b9ac91c41d79
Shohei <Shohei@10.242.17.214>
2018-06-12 23:29:40 +0900

    next

--- IntroBioinfo/Chap2.py
+++ IntroBioinfo/Chap2.py
@@ -4,5 +4,13 @@ Created on 2018/06/12
 @author: Shohei
 '''
 
-print("hello world")
-print("こんにちわ")
+is_cont=True
+
+while is_cont:
+    print('press any key')
+    c = input()
+    
+    if c == 'end':
+        is_cont=False
+    else:
+        print('You pressed '+c)
