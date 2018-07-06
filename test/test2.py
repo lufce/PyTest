2ae2f25c161b5c68c70f4c9e955a8fa493132264
shohei_desk <shohei_desk@192.168.10.7>
2018-07-06 09:53:46 +0900

    a

--- test/test2.py
+++ test/test2.py
@@ -6,8 +6,9 @@ Created on 2018/07/05
 
 import requests as webs
 
-res = webs.get("https://www.cell.com/cell/fulltext/S0092-8674(18)30713-X")
+res = webs.get("http://emboj.embopress.org/content/early/2018/07/04/embj.201899264")
 res.encoding = res.apparent_encoding
-f1 = open("cell_article2.html", "w", encoding = "UTF-8")
+f1 = open("emboj_article.html", "w", encoding = "UTF-8")
+# f1 = open("emboj_newarticles.html", "w", encoding = "UTF-8")
 f1.write(res.text)
 f1.close()
