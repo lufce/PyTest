c09146fb4dfa3d28b01f1c3001d18c103465dfe1
shohei_desk <shohei_desk@192.168.10.7>
2018-10-17 06:30:59 +0900

    a

--- test/test2.py
+++ test/test2.py
@@ -6,9 +6,9 @@ Created on 2018/07/05
 
 import requests as webs
 
-res = webs.get("http://emboj.embopress.org/content/early/2018/07/04/embj.201899264")
+res = webs.get("https://www.nature.com/ncb/")
 res.encoding = res.apparent_encoding
-f1 = open("emboj_article.html", "w", encoding = "UTF-8")
+f1 = open("ncb_top.html", "w", encoding = "UTF-8")
 # f1 = open("emboj_newarticles.html", "w", encoding = "UTF-8")
 f1.write(res.text)
 f1.close()
